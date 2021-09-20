b5b367ace04f26f0c25d442e279623d5426a73b1
Kelly <kellytso04@gmail.com>
2021-09-20 17:24:46 -0400

    Implemented getElementsByClassName.js & stringifyJSON.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,29 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// I: a string representing our target class name
+// O: an array of elements with class names that match target class name
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+  // Isolate the head node of the DOM
+  var root = document.body;
+
+  var innerFunction = function(node) {
+    // Check if the head node contains the target className
+    if (node.classList && node.classList.contains(className)) {
+      result.push(node);
+    }
+
+    // if head node has child nodes...
+    if (node.hasChildNodes()) {
+      for (var i = 0; i < node.childNodes.length; i++) {
+        innerFunction(node.childNodes[i]);
+      }
+    }
+  };
+  innerFunction(root);
+
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,64 @@
 
 // but you don't so you're going to write it from scratch:
 
+// I: a javascript data type
+// O: the stringified version of the input value
+// E: if obj === function || obj === undefined, we're going to return an empty object literal
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log(obj);
+
+  if (typeof obj === 'string') {
+    console.log(obj + ' is a string');
+    return '"' + obj + '"';
+  } else if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'object') { // [8],
+    console.log('obj is a collection');
+
+    if (Array.isArray(obj)) {
+      var result = '[';
+
+      for (var i = 0; i < obj.length; i++) {
+        if (i !== obj.length - 1) {
+          result += stringifyJSON(obj[i]) + ',';
+        } else {
+          result += stringifyJSON(obj[i]);
+        }
+      }
+
+      result += ']';
+      return result;
+    } else {
+      // edge cases: functions, undefined
+      if (!Object.keys(obj).length) {
+        return '{}';
+      }
+
+      var result = '{';
+
+      for (var key in obj) {
+
+        if ((typeof obj[key] === 'function') || (typeof obj[key] === 'undefined')) {
+          continue;
+        } else {
+          result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ','; // {'key' : 'value'}
+        }
+
+
+      }
+
+      if (result.length > 1) {
+        result = result.slice(0, result.length - 1);
+      }
+
+      result += '}';
+      return result;
+    }
+  } else {
+    console.log('obj is none of the above');
+
+    return obj + '';
+  }
+
 };
